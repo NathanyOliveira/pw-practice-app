492194a6b74eccb2885679aa5a47d28701bea40b
Nathany <naty_om_3@hotmail.com>
2026-04-21 23:07:41 +0200

    locator syntax rules

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -2,28 +2,58 @@ import { test } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4200/')
+  await page.getByText('Forms').click()
+  await page.getByText('Form Layouts').click()
 })
 
-test.describe('Forms section', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.getByText('Forms').click()
-  })
+// test.describe('Forms section', () => {
+//   test.beforeEach(async ({ page }) => {
+//     await page.getByText('Forms').click()
+//   })
 
-  test('go to Form Layouts', async ({ page }) => {
-    await page.getByText('Form Layouts').click()
-  })
+//   test('go to Form Layouts', async ({ page }) => {
+//     await page.getByText('Form Layouts').click()
+//   })
 
-  test('go to Datepicker', async ({ page }) => {
-    await page.getByText('Datepicker').click()
-  })
+//   test('go to Datepicker', async ({ page }) => {
+//     await page.getByText('Datepicker').click()
+//   })
+// })
+
+// test.describe('Charts section', () => {
+//   test.beforeEach(async ({ page }) => {
+// await page.getByText('Charts').first().click()  })
+
+//   test('open charts page', async ({ page }) => {
+//     await page.getByText('Echarts').click()
+//   })
+// })
+
+test('Locator sintax rules', async({page}) =>{
+  //by Tag name
+  //await page.locator('input').first().click()
+
+  //by ID
+ await page.locator('#inputEmail1').click()
+
+  //by Class value
+  page.locator('.shape-rectangle')
+
+  //by Attribute 
+  page.locator('[placeholder="Email"]')
+
+  //by Class value (full)
+  page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+  //Combine different selectors
+  page.locator('input[placeholder="Email"]')
+
+  //by XPath(NOT RECOMMENDED)
+  page.locator('//*[@id=inputEmail1]')
+
+  //by Partial text match 
+  page.locator(':text("Using")')
+
+  //by Exact text match 
+  page.locator(':text-is("Using the Grid")')
 })
-
-test.describe('Charts section', () => {
-  test.beforeEach(async ({ page }) => {
-await page.getByText('Charts').first().click()  })
-
-  test('open charts page', async ({ page }) => {
-    await page.getByText('Echarts').click()
-  })
-})
-
